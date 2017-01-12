22138dabba4b49519548832e43b879254f22625b
Piotr Ranosz <pranosz@wp.pl>
2017-01-12 20:07:06 +0100

    Project init
    First commit. Clean directories and files.

---
+++ js/ediTable.js
@@ -0,0 +1,14 @@
+/*
+ * EdiTable
+ * 
+ * To do - describe component
+ * 
+ */
+
+/*
+ * @param:String {tableId} id attribute of table in which we want use "EdiTable" component
+ * @param:Object {tableSet} a set of parameters that we want to send to "EdiTable" component
+ */
+function EdiTable(tableId, tableSet){
+    
+}
