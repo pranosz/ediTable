00267c4f3267561814d5f8b3b3b1b2da9a5bacb8
Piotr Ranosz <pranosz@wp.pl>
2017-01-17 22:50:40 +0100

    Added configurations
    Change in ediTable.js:
    1. Added _defaultSet method which contains default settings;
    2. Added _setConfig method which compare default config with user config
    and returns updated version;
    3. Added ediTable.css file with temporary css styles;
    4. Added events.js file.

--- js/ediTable.js
+++ js/ediTable.js
@@ -3,12 +3,47 @@
  * 
  * To do - describe component
  * 
- */
-
-/*
  * @param:String {tableId} id attribute of table in which we want use "EdiTable" component
  * @param:Object {tableSet} a set of parameters that we want to send to "EdiTable" component
  */
+"use strict";
+
 function EdiTable(tableId, tableSet){
+    var _tId = tableId;
+    var config = this._setConfig(tableSet);
     
-}
+    console.log("config: "+JSON.stringify(config));
+
+};
+
+/*
+ * Default settings
+ */
+EdiTable.prototype._defaultSet = {
+    editType: "context", // buttons, none
+    sorting: true, // false
+    exportCSV: false, // false
+    defaultCss: true, // false
+    colToSort: [], // choice column to sort (optional property)
+    pagination: true, //false
+    language: "pl" // "eng"
+};
+
+/*
+ * _setConfig - compare user settings with default settings.
+ * If user doesn't set one of the values then it's taken from 
+ * defaults config.
+ * 
+ * @param:object {tableSet} user config
+ * @return:object {defaultCopy} updated the default configuration of user configuration
+ */
+EdiTable.prototype._setConfig = function(tableSet){
+    var defaultCopy = JSON.parse(JSON.stringify(this._defaultSet));
+    
+    for(var opt in defaultCopy){
+        if(opt in tableSet){
+            defaultCopy[opt] = tableSet[opt];
+        } 
+    }
+    return defaultCopy;
+};
