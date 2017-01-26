a85965ac3dcf94523b61882d66db0607cb1443fa
Piotr Ranosz <pranosz@wp.pl>
2017-01-26 23:37:40 +0100

    Added some methods and styles related to context menu
    Changes in ediTable.js file:
    1.  Changes in "_addContextmenu" method / changed conception of adding
    events. Added variable blueprints. Launched two methods:
    _drawContextmenu and _addEventContextmenu;
    2. Added method _drawContextmenu which draw contextmenu using blueprints
    variable;
    3. Added _addEventContextmenu method which adds event "contextmenu" and
    runs _showContexmenu method;
    4. Added _showContexmenu method which hide or show contextmenu;
    5. Added _setContextPosition method which set position of contextmenu;
    6. Added _addEventsToButtons method which adds events "click" to
    contextmenu buttons.

--- js/ediTable.js
+++ js/ediTable.js
@@ -9,7 +9,8 @@
 "use strict";
 
 function EdiTable(tableId, tableSet){
-    this._tId = tableId;
+    this._table = tableId;
+    this._contextmenu = null;
     var config = this._setConfig(tableSet);
     
     for(var opt in config){
@@ -76,10 +77,8 @@ EdiTable.prototype._setConfig = function(tableSet){
  */
 EdiTable.prototype._setEditType = function(ediType){
     if(ediType === "context"){
-        console.log("_setEditType: context");
         this._addContextmenu();
     }else if(ediType === "buttons"){
-        console.log("_setEditType: addBtns");
         this._addBtns();
     }else {
         //none
@@ -125,42 +124,154 @@ EdiTable.prototype._setLanguage = function(){
  * _addRow
  */
 EdiTable.prototype._addRow = function(side){
-    
+    console.log("_addRow "+side);
 };
 /*
  * _delRow
  */
 EdiTable.prototype._delRow = function(){
-    
+    console.log("_delRow ");
 };
 /*
  * _addCol
  */
 EdiTable.prototype._addCol = function(side){
-    
+    console.log("_addCol "+side);
 };
 /*
  * _delCol
  */
 EdiTable.prototype._delCol = function(){
+    console.log("_delCol ");
+};
+/*
+ * _addEventsToButtons
+ */
+EdiTable.prototype._addEventsToButtons = function(){
+    var owner = this;
+    document.querySelector("body").addEventListener("click",function(e){
+    var node = e.target.classList.item(1);
+        switch(node){
+            case "button-add-row-up":
+                owner._addRow("U");
+            break;
+            case "button-add-row-down":
+                owner._addRow("D");
+            break;
+            case "button-del-row":
+                owner._delRow();
+            break;
+            case "button-add-col-right":
+                owner._addCol("R");
+            break;
+            case "button-add-col-left":
+                owner._addCol("L");
+            break;           
+            case "button-del-col":
+                owner._delCol();
+            break;
+        }
+    },false);
+};
+/*
+ * _showMenu
+ */
+EdiTable.prototype._showContexmenu = function(){ 
+    if(this._t){
+        this._t = 0;
+        this._contextmenu.style.display = "block";
+    }else {
+        this._t = 1;
+        this._contextmenu.style.display = "none";
+    }
+};
+/*
+ * _drawContextmenu
+ */
+EdiTable.prototype._drawContextmenu = function(btns){
+    var div = document.createElement("div");
+    var ul = document.createElement("ul");
     
+    div.classList.add("context");
+    ul.classList.add("context-list");
+    
+    btns.forEach(function(btn,i){
+        var li = document.createElement("li");
+        var b = document.createElement("button");
+        var text = document.createTextNode(btn.label);
+        b.classList.add("button");
+        b.classList.add(btn.class);
+        b.appendChild(text);
+        li.appendChild(b);
+        ul.appendChild(li);
+    });
+    div.appendChild(ul);
+    this._table.parentNode.insertBefore(div,null);
+    this._contextmenu = document.querySelector(".context");    
+};
+/*
+ * _addEventContextmenu
+ */
+EdiTable.prototype._addEventContextmenu = function(){
+    
+    this._t = 1;
+    this._selectedRow = null;
+    this._selectedCol = null;
+    var owner = this;
+    
+    this._table.addEventListener("contextmenu",function(e){
+        var node = e.target.nodeName;
+        var btnDis = document.querySelector(".button-add-row-up");
+        if(node === "TH" || node === "TD"){
+            e.preventDefault();
+            owner._setContextPosition(e);
+            owner._selectedRow = e.target.parentNode.rowIndex;
+            owner._selectedCol = e.target.cellIndex;
+            if(node === "TH"){
+                btnDis.classList.remove("button");
+                btnDis.classList.add("button-disabled");
+                btnDis.disabled = true;
+            }else if(node === "TD"){
+                btnDis.classList.remove("button-disabled");
+                btnDis.classList.add("button");
+                btnDis.disabled = false;
+            }
+            owner._showContexmenu();
+        }
+    },false);
+    
+    window.addEventListener("click",function(){ 
+        owner._t === 0 ? owner._showContexmenu():null;
+    },false);
+    
+    window.onresize = function(){
+        owner._t = 1;
+        owner._contextmenu.style.display = "none";
+    };  
+    this._addEventsToButtons();
+};
+/* 
+ * _setContextPosition
+ */
+EdiTable.prototype._setContextPosition = function(eventObj){
+    this._contextmenu.style.position = "absolute";
+    this._contextmenu.style.left = eventObj.clientX+"px";
+    this._contextmenu.style.top = eventObj.clientY+"px";
 };
 /*
  * _addContextmenu
  */
-EdiTable.prototype._addContextmenu = function(){
-    var target = this._tId,
-        type = "contextmenu",
-        useCapture = false,
-        callback = [
-        {btn: "addColBtn",function: this.addCol,params: ["L"]},
-        {btn: "delColBtn",function: this.delCol},
-        {btn: "addColBtn",function: this.addCol,params: ["R"]},
-        {btn: "addRowBtn",function: this.addRow,params: ["U"]},
-        {btn: "delRowBtn",function: this.delRow},
-        {btn: "addRowBtn",function: this.addRow,params: ["D"]}
+EdiTable.prototype._addContextmenu = function(){ 
+    var blueprints = [
+    {class: "button-add-row-up", label: "Add row above",function: this._addRow},
+    {class: "button-add-row-down", label: "Add row below",function: this._addRow},
+    {class: "button-del-row", label: "Delete row",function: this._delRow},
+    {class: "button-add-col-right", label: "Add column to right",function: this._addCol},
+    {class: "button-add-col-left", label: "Add column to left",function: this._addCol},
+    {class: "button-del-col", label: "Delete column",function: this._delCol}
     ];
-    Event.add(target,type,callback,useCapture); 
+    this._drawContextmenu(blueprints);
+    this._addEventContextmenu(); 
 };
 /*
  * _addBtns
--- js/event.js
+++ js/event.js
@@ -5,8 +5,12 @@
 var Event = {
     add:function(target,type,callback,useCapture){
             var paramsLength = callback.length;
-            console.log("paramsLength "+paramsLength);
             target.addEventListener(type,function(e){
+                callback.forEach(function(btn,i){
+                    if(btn === e.target.NodeName){
+                        
+                    }
+                });
             },useCapture);
         },
     remove:function(target,type,callback,useCapture){
